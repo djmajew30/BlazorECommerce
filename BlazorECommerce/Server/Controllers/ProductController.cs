7f18db86c3f399b5051e814fea878895905c78e1
DavidMa <DavidMa@P17-PF2PTX0W>
2022-11-29 11:38:42 -0500

    Add project files.

---
+++ BlazorECommerce/Server/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorECommerce.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        //get products from DB
+        public ProductController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProduct() //shows schema in swagger/index.html
+        {
+            //access products in db
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+    }
+}
